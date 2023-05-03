c522008065d51251cc161a83d816c59a46cd6650
Alexey Borsky <borsky.alexey@gmail.com>
2023-05-03 14:08:06 +0300

    path references updated

--- scripts/base_ui.py
+++ scripts/base_ui.py
@@ -4,7 +4,8 @@ basedirs = [os.getcwd()]
 for basedir in basedirs:
     paths_to_ensure = [
         basedir,
-        basedir + '/extensions/sd-cn-animation/scripts'
+        basedir + '/extensions/sd-cn-animation/scripts',
+        basedir + '/extensions/SD-CN-Animation/scripts'
         ]
 
     for scripts_path_fix in paths_to_ensure:
